621aa594db24a39d6b345406b6bd71caff7f1078
ProjectSoft <projectsoft2009@yandex.ru>
2024-06-19 20:52:45 +0400

    update

--- assets/plugins/filemanageropen/lang/english.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$lang = array();
-$lang["images"]		= "Images";
-$lang["files"]		= "Files";
-$lang["media"]		= "Media files";
-$lang["copy"]		= "Copied";
-$lang["nocopy"]		= "Your browser does not support copying to the clipboard";
